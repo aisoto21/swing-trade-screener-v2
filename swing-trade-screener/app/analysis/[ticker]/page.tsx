aa53eae3a40da47eeaf8c0c08172ccbcc231cc43
aisoto21 <adrianisoto6@gmail.com>
2026-03-13 17:55:57 -0500

    analysis fix

--- swing-trade-screener/app/analysis/[ticker]/page.tsx
+++ swing-trade-screener/app/analysis/[ticker]/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import AnalysisPageClient from "./AnalysisPageClient";
+import AnalysisPageClient from "./AnalysisPageClient.tsx";
 
 export default function AnalysisPage({
   params,
